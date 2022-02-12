a95f2d6735399be1dafcb4824fb18500282e4b02
shellieD <shellie.downie13@gmail.com>
2022-02-12 00:03:24 +0000

    testy

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
-let numbers = [];
-for (let i = 0; i < 10; i++) {
-    numbers.push(Math.ceil(Math.random() * 10));
+
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'maths','english', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', english: 'Joan', art: 'Simon'},
+        results: {science: 93, maths: 95, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 63, maths: 79, art: 95},
+    }
+];
+
+const averagePoints = (arr, subject) => {
+    let sum = 0;
+    let count = 0;
+    for (let student of arr){
+        if (student.subjects.includes(subject)) {
+            sum = sum + student.results[subject];
+            count++;
+        }
+    }
+    return sum / count;
 }
 
-console.log(numbers);
+let result = averagePoints(students, 'english');
+console.log(result)
