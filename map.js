cad1168a102b5dfd4ce0fb1db33e1d873f07e47d
shellieD <shellie.downie13@gmail.com>
2022-02-12 16:56:55 +0000

    Add map.js

---
+++ map.js
@@ -0,0 +1,9 @@
+// The map() operator
+//returns a new array containing all results of a callback function
+// A callback function is a function you want to execute on every element in the array
+//EXAMPLE
+
+let nums = [1, 2, 3, 4, 5, 6];
+const results = nums.map(num => num * 2);
+
+console.log(results);
