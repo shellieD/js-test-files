2b77e5dcfb85422340d69cca52d6df66ad0e39bd
shellieD <shellie.downie13@gmail.com>
2022-02-12 16:56:48 +0000

    Add filter.js

---
+++ filter.js
@@ -0,0 +1,14 @@
+//the filter() method
+//iterates over an array of items and filters the array to only a specified set of results
+// EXAMPLE
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let evens = nums.filter(n => n % 2 === 0);
+let odds = nums.filter(n => n % 2 !== 0);
+console.log(evens);
+console.log(odds);
+
+let names = ['mike', 'jill', 'matt', 'jenny'];
+let mNames = names.filter(name => name.charAt(0) === 'm');
+
+console.log(mNames); 
